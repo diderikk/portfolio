ebe4aea2dafc77da89d2ebede096c73b88755693
Diderik Kramer <diderikk@stud.ntnu.no>
2023-02-24 21:34:26 +0100

    fix and simplify content table reader

--- src/utils/html-reader.ts
+++ src/utils/html-reader.ts
@@ -3,33 +3,15 @@ import { ContentTableItem } from "../types/content-item.type";
 export const getContentTable = (htmlString: string): ContentTableItem[] => {
   const contentTable: ContentTableItem[] = [];
   const headerRegex = /^<h(\d)\s*id="[A-Za-z0-9\-]+">([A-Za-z0-9\- ]+)/gim;
-  const listMember = /^<li><p>([A-Za-z0-9 \-\_]*)/gim;
   const matches = Array.from(htmlString.matchAll(headerRegex));
 
-  const splitted = htmlString.split("\n");
-  let currentHeaderIndex = 0;
-  let insideOrderedList = false;
-  let orderListIndex = 1;
-
-  splitted.forEach((line) => {
-    if (headerRegex.test(line)) {
-      contentTable.push({
-        item: capitalize(matches[currentHeaderIndex][2]),
-        indent: matches[currentHeaderIndex++][1] === "3",
-      });
-    } else if (line.includes("ol")) {
-      insideOrderedList = !insideOrderedList;
-      orderListIndex = 1;
-    } else if (listMember.test(line) && insideOrderedList) {
-      contentTable.push({
-        item: `${orderListIndex++}. ${Array.from(
-          line.match(listMember)!
-        )[0].replace("<li><p>", "")}`,
-        indent: true,
-      });
-    }
-  });
-
+  matches.forEach(match => {
+    contentTable.push({
+      item: capitalize(match[2]),
+      indent: match[1] === "3",
+    });
+  })
+  
   return contentTable;
 };
 
